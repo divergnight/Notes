e7ef38662df82962de23e96a087c9f7dfac3cff4
divergnight <47478344+divergnight@users.noreply.github.com>
2022-02-04 18:52:42 +0100

    Complete Page Add Note

--- src/Components/NoteActionForm/NoteActionForm.jsx
+++ src/Components/NoteActionForm/NoteActionForm.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Card, Form, InputGroup, FormControl, Container, Row, Col } from 'react-bootstrap';
+import { Card, Form, InputGroup, FormControl, Container, Row, Col, Button } from 'react-bootstrap';
 import { useNavigate, Link } from 'react-router-dom';
 import { CurrentProvider } from '../../Providers/CurrentProvider';
 import { NotebookProvider } from '../../Providers/NotebookProvider';
@@ -11,14 +11,13 @@ import IconShow from '../Icons/IconShow';
 import IconHide from '../Icons/IconHide';
 
 export default function NoteActionForm(props) {
-	const [addForm, setAddForm] = useState({ title: '', content: '' });
+	const form = props.form;
+	const setForm = props.setForm;
 	const [isPreview, setIsPreview] = useState(false);
 	const action = props.action;
 
 	const navigate = useNavigate();
 
-	const currentProvider = new CurrentProvider();
-	const notebookProvider = new NotebookProvider(currentProvider.get().notebook);
 	const notebooksProvider = new NotebooksProvider();
 	const categories = notebooksProvider.get();
 
@@ -26,17 +25,17 @@ export default function NoteActionForm(props) {
 	const DOMPurify = require('dompurify')(window);
 
 	function changeValue(key, e) {
-		let tmp = { ...addForm };
+		let tmp = { ...form };
 		tmp[key] = e.target.value;
-		setAddForm(tmp);
+		setForm(tmp);
 	}
 	function clean() {
-		return DOMPurify.sanitize(converter.makeHtml(addForm.content));
+		return DOMPurify.sanitize(converter.makeHtml(form.content));
 	}
 
 	return (
 		<Card id="NoteActionForm-card" style={{ width: isPreview ? '70%' : '90%' }}>
-			<Form>
+			<Form onSubmit={e => action(e)}>
 				<Card.Header>
 					<Card.Title id="NoteActionForm-title">Add note</Card.Title>
 					<span id="NoteActionForm-Back" onClick={() => navigate('./..')}>
@@ -47,58 +46,66 @@ export default function NoteActionForm(props) {
 					</span>
 				</Card.Header>
 				{!isPreview && (
-					<Card.Body>
-						<Container fluid>
-							<Row>
-								<Col xxl={6}>
-									<Container fluid className="mb-3" id="NoteActionForm-Form">
-										<Row>
-											<Col xs={7}>
-												<InputGroup>
-													<InputGroup.Text>Title</InputGroup.Text>
-													<FormControl
-														placeholder="Enter note title"
-														value={addForm.title}
-														onChange={e => changeValue('title', e)}
-														required
-													/>
-												</InputGroup>
-											</Col>
-											<Col xs={5}>
-												<InputGroup className="mb-3">
-													<InputGroup.Text>Category</InputGroup.Text>
-													<Form.Select aria-label="Default select example">
-														{categories.map(category => (
-															<option key={category.id} value={category.id}>
-																{category.name}
-															</option>
-														))}
-													</Form.Select>
-												</InputGroup>
-											</Col>
-										</Row>
-									</Container>
-									<InputGroup id="NoteActionForm-TextArea">
-										<InputGroup.Text>Text</InputGroup.Text>
-										<FormControl
-											as="textarea"
-											placeholder="Your Markdown code"
-											value={addForm.content}
-											onChange={e => changeValue('content', e)}
-											rows="20"
-										/>
-									</InputGroup>
-								</Col>
-								<Col xs={12} xxl={6}>
-									{clean().length != 0 && (
+					<>
+						<Card.Body>
+							<Container fluid>
+								<Row>
+									<Col xxl={6}>
+										<Container fluid className="mb-3" id="NoteActionForm-Form">
+											<Row>
+												<Col xs={7}>
+													<InputGroup>
+														<InputGroup.Text>Title</InputGroup.Text>
+														<FormControl
+															placeholder="Enter note title"
+															value={form.title}
+															onChange={e => changeValue('title', e)}
+															required
+														/>
+													</InputGroup>
+												</Col>
+												<Col xs={5}>
+													<InputGroup className="mb-3">
+														<InputGroup.Text>Category</InputGroup.Text>
+														<Form.Select aria-label="Default select example">
+															{categories.map(category => (
+																<option key={category.id} value={category.id}>
+																	{category.name}
+																</option>
+															))}
+														</Form.Select>
+													</InputGroup>
+												</Col>
+											</Row>
+										</Container>
+										<InputGroup id="NoteActionForm-TextArea">
+											<InputGroup.Text>Text</InputGroup.Text>
+											<FormControl
+												as="textarea"
+												placeholder="Your Markdown code"
+												value={form.content}
+												onChange={e => changeValue('content', e)}
+												rows="20"
+											/>
+										</InputGroup>
+									</Col>
+									<Col xs={12} xxl={6}>
 										<div id="NoteActionForm-Live-Preview">
 											<div dangerouslySetInnerHTML={{ __html: clean() }}></div>
 										</div>
-									)}
-								</Col>
-							</Row>
-						</Container>
-					</Card.Body>
+									</Col>
+								</Row>
+							</Container>
+						</Card.Body>
+						<Card.Footer>
+							<Button id="NoteActionForm-Save" type="submit">
+								Save
+							</Button>
+							<Button variant="secondary" id="NoteActionForm-Cancel" onClick={() => navigate('./..')}>
+								Cancel
+							</Button>
+						</Card.Footer>
+					</>
 				)}
 				{isPreview && (
 					<div id="NoteActionForm-Preview">
--- src/Pages/AddNotePage/AddNotePage.jsx
+++ src/Pages/AddNotePage/AddNotePage.jsx
@@ -1,17 +1,34 @@
 import { useEffect, useState } from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 import AppNavbar from '../../Components/AppNavbar/AppNavbar';
 import NoteActionForm from '../../Components/NoteActionForm/NoteActionForm';
+import { CurrentProvider } from '../../Providers/CurrentProvider';
+import { NotebookProvider } from '../../Providers/NotebookProvider';
 import './AddNotePage.css';
 
 export default function AddNotePage() {
+	const [addForm, setAddForm] = useState({ title: '', content: '' });
+	const navigate = useNavigate();
+
+	const currentProvider = new CurrentProvider();
+	const notebookProvider = new NotebookProvider(currentProvider.get().notebook);
+
+	function add(e) {
+		e.preventDefault();
+
+		notebookProvider.add(addForm);
+
+		navigate('./..');
+	}
+
 	return (
 		<div>
 			<AppNavbar />
 			<main>
 				<Container fluid>
 					<Row>
-						<NoteActionForm action={'add'} />
+						<NoteActionForm action={add} form={addForm} setForm={setAddForm} />
 					</Row>
 				</Container>
 			</main>
